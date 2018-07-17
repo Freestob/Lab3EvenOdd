6b3a3e2b1bf0083a34613579f8933dc8fdcd69a5
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-17 16:12:50 -0400

    Added validation to function to check if numbers are within 1 and 100

--- Lab3EvenOdd/Program.cs
+++ Lab3EvenOdd/Program.cs
@@ -12,33 +12,48 @@ namespace Lab3EvenOdd
         {
             while (true)
             {
-                int userNumber = ConvertUserNumber();
+                string userName;
+
+                Console.WriteLine("What is your name?");
+                userName = Console.ReadLine();
+
+                Console.WriteLine("Hello " + userName + ", today we are going to find out if a number is even or odd.");
+
+                int userNumber = ConvertUserNumber();                        
+                
                 if (userNumber % 2 == 0 && Enumerable.Range(2, 25).Contains(userNumber))
                 {
-                    Console.WriteLine(userNumber + " Even and less than 25.");
+                    Console.WriteLine(userName + ", " + userNumber + " is even and less than 25.");
                     Console.ReadLine();
                 }
                 else if (userNumber % 2 == 0 && Enumerable.Range(26, 60).Contains(userNumber))
                 {
-                    Console.WriteLine("Even");
+                    Console.WriteLine(userName + ", that number is even");
                     Console.ReadLine();
                 }
                 else if (userNumber % 2 == 0 && Enumerable.Range(61, 100).Contains(userNumber))
                 {
-                    Console.WriteLine(userNumber + " Even");
+                    Console.WriteLine(userName + ", " + userNumber + " is even");
                     Console.ReadLine();
                 }
                 else
                 {
-                    Console.WriteLine(userNumber + " Odd");
+                    Console.WriteLine(userName + ", " + userNumber + " is Odd");
                     Console.ReadLine();
                 }
                 Console.WriteLine("Do you want to run again (Y/N)? ");
 
                 if (Console.ReadLine().ToLower() != "y")
+                {
                     Console.WriteLine("Thank you for playing");
                     Console.ReadLine();
                     break;
+                }
+                else
+                {
+                    return;
+                }
+
             }
         }
 
@@ -47,16 +62,21 @@ namespace Lab3EvenOdd
         {
             string stringNumber;
             int userNumber;
-            Console.WriteLine("\n" + "Please enter in a number between 1 and 100.");
+            Console.WriteLine("\n" + "Please enter in a valid number between 1 and 100.");
             stringNumber = Console.ReadLine();
             bool succes = int.TryParse(stringNumber, out userNumber);
-            if (succes == true)
+            if (!succes)
             {
-                return userNumber;
-            }
-            else
                 Console.WriteLine("\n" + "Please enter in a valid number");
-            return ConvertUserNumber();
+                return ConvertUserNumber();
+            }
+            if (userNumber >=101 || userNumber <= 0)
+            {
+                Console.WriteLine("Please enter in a valid number between 1 and 100.");
+                return ConvertUserNumber();
+            }
+            return userNumber;
+
         }
     }
 }
