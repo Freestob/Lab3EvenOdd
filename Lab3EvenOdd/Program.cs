9a432cb4cd710c508beecb0f3be0a9203b647592
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-17 15:09:29 -0400

    Added else and else if to meet requirements

--- Lab3EvenOdd/Program.cs
+++ Lab3EvenOdd/Program.cs
@@ -10,6 +10,44 @@ namespace Lab3EvenOdd
     {
         static void Main(string[] args)
         {
+           int userNumber = ConvertUserNumber();
+            if (userNumber % 2 == 0 && Enumerable.Range(2, 25).Contains(userNumber))
+            {
+                Console.WriteLine(userNumber + " Even and less than 25.");
+                Console.ReadLine();
+            }
+            else if (userNumber % 2 == 0 && Enumerable.Range(26, 60).Contains(userNumber))
+            {
+                Console.WriteLine("Even");
+                Console.ReadLine();
+            }
+            else if (userNumber % 2 == 0 && Enumerable.Range(61, 100).Contains(userNumber))
+            {
+                Console.WriteLine(userNumber + " Even");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(userNumber + " Odd");
+                Console.ReadLine();
+            }
+        }
+
+
+        static int ConvertUserNumber()
+        {
+            string stringNumber;
+            int userNumber;
+            Console.WriteLine("\n" + "Please enter in a number between 1 and 100.");
+            stringNumber = Console.ReadLine();
+            bool succes = int.TryParse(stringNumber, out userNumber);
+            if (succes == true)
+            {
+                return userNumber;
+            }
+            else
+                Console.WriteLine("\n" + "Please enter in a valid number");
+            return ConvertUserNumber();
         }
     }
 }
